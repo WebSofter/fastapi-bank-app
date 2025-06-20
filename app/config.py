1ebf2eda495ee40d16712e9f916c5ab69dd3c16c
David Amirkhanov <mail.websofter@gmail.com>
2025-06-20 15:15:57 +0300

    updt

--- app/config.py
+++ app/config.py
@@ -1,23 +1,22 @@
-from pydantic import Field
+from pydantic import Field, ConfigDict
 from pydantic_settings import BaseSettings
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 class Settings(BaseSettings):
+    model_config = ConfigDict(env_file=".env", extra='allow')
+    
     database_url: str = Field(alias="DATABASE_URL")
     secret_key: str = Field(alias="SECRET_KEY")
     algorithm: str = Field(default="HS256", alias="ALGORITHM")
     access_token_expire_minutes: int = 30
     
-    # Добавьте эти поля, если они нужны
+    # Database configuration fields
     db_user: str = Field(alias="DB_USER")
     db_password: str = Field(alias="DB_PASSWORD") 
     db_name: str = Field(alias="DB_NAME")
     db_port: str = Field(default="5432", alias="DB_PORT")
     db_host: str = Field(default="localhost", alias="DB_HOST")
-    
-    class Config:
-        env_file = ".env"
 
 settings = Settings()
